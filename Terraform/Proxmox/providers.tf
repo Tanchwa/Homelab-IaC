39f726fd11c310d926d5b0d88161c261c6979e1b
Andrew Sutliff <dres2dogs@gmail.com>
2024-04-30 16:06:17 -0400

    added base files for proxmox home assistant module, added git ignore to ignore local tfvars file while testing

--- Terraform/Proxmox/providers.tf
+++ Terraform/Proxmox/providers.tf
@@ -1,20 +1,27 @@
 terraform {
-  required_version = "value"
-  backend "azurerm" {
-    #backend set during terraform init
-  }
+  required_version = "~> 1.8.0"
+  #backend "azurerm" {
+  #  #backend set during terraform init
+  #}
 
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "~> 1.0"
+      version = "~> 0.54.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url      = var.pm_api_url
-  pm_user         = var.pm_user
-  pm_password     = var.pm_password
-  pm_tls_insecure = true
+  endpoint  = "https://172.31.0.200:8006/"
+  api_token = var.proxmox_api_token
+  insecure  = true
 }
