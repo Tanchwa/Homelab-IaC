1e3be7fc415cfb89a6b85987956c94d08d6b3f94
Andrew Sutliff <dres2dogs@gmail.com>
2025-02-04 11:44:33 -0500

    added grafana alert template

--- Terraform/Grafana/main.tf
+++ Terraform/Grafana/main.tf
@@ -3,7 +3,25 @@ resource "grafana_contact_point" "line" {
 
   line {
     token       = var.line_jwt
-    description = "Line Notification to Personal Account"
     title       = "Grafana Kubernetes Alert"
+    description = "{{ template \"custom.alerts.line\" .}}"
   }
+}
+
+resource "grafana_message_template" "line" {
+  name = "custom.alerts"
+
+  template = <<EOT
+ {{ define "custom.alerts.line" -}}
+{{ len .Alerts }} alert(s)
+{{ range .Alerts -}}
+  {{ template "alert.summary_and_description" . -}}
+{{ end -}}
+{{ end -}}
+{{ define "alert.summary_and_description" }}
+  Summary: {{.Annotations.summary}}
+  Status: {{ .Status }}
+  Description: {{.Annotations.description}}
+{{ end -}}
+EOT
 }
